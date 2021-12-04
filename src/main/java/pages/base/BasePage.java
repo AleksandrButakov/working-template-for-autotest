21c9f0f3a480b40050be2bd92f46f7b3c7ab6611
GTemp <anbngm@gmail.com>
2021-12-04 23:59:46 +0500

    base result

--- src/main/java/pages/base/BasePage.java
+++ src/main/java/pages/base/BasePage.java
@@ -1,14 +1,29 @@
 package pages.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static constants.Constant.TimeoutVariables.EXPLICIT_WAIT;
 
+// в BasePage будут находиться методы по назначению одинаковые для всех страниц
 public class BasePage {
 
     WebDriver driver;
     // создадим конструктор
     public BasePage(WebDriver driver) {
         this.driver = driver;
+    }
 
+    // метод перехода к определенному url
+     public void goToUrl(String url) {
+         driver.get(url);
+     }
+
+     // метод по работе с явными ожиданиями
+    public WebElement waitIsVisible(WebElement element) {
+        new WebDriverWait(driver, EXPLICIT_WAIT).until(ExpectedConditions.visibilityOf(element));
+        return element;
     }
 }
