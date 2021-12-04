ae6f3562daf27a8a572d79308f87d9080fd81014
GTemp <anbngm@gmail.com>
2021-12-04 23:41:41 +0500

    next step

---
+++ src/main/java/common/Config.java
@@ -0,0 +1,8 @@
+package common;
+
+public class Config {
+    // создадим переменную для очистки cookies, если true, то cookies чистить не нужно
+    public static final Boolean CLEAR_COOKIES = true;
+    public static final Boolean HOLD_BROWSER_OPEN = true;
+
+}
--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -1,12 +1,30 @@
 package tests.base;
 
 import common.CommonActions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import pages.base.BasePage;
 import pages.car_loans.CarLoansPage;
 
+import static common.Config.CLEAR_COOKIES;
+
+/*
+Эта аннотация говорит что все аннотации в BaseTest они будут выполняться после прогона каждого класса. Единственным
+минусом такой архитектуры теста будет являться то, что после выполнения определенного тестового класса
+car_loans например, у нас будет отрываться браузер и потом браузер будет закрываться.
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
 // здесь будет происходить инициализация драйвера
 public class BaseTest {
+    /*
+    Помимо создания и инициализации драйвера в tests.base у нас будут лежать в этом классе вспомогательные методы
+    по работе с жизненным циклом test. Для того чтобы реализовать работу с жизненным циклом нам необходимо добавить
+    аннотацию @TestInstance
+    */
 
     // в BaseTest мы создадим драйвер через CommonActions.createDriver(), проинициализируем его и прокинем в
     // конструктор страницы BasePage. Аналогичным образом будем прокидывать его в конструкторы всех остальных страниц
@@ -15,5 +33,24 @@ public class BaseTest {
     protected BasePage basePage = new BasePage(driver);
     protected CarLoansPage carLoansPage = new CarLoansPage(driver);
 
+    @AfterEach
+    // после каждого теста будем чистить cookies, чтоб каждый тест начинался как с нуля
+    void clearCookiesAndLocalStorage() {
+        if (CLEAR_COOKIES) {
+            // если true, то удаляем cookies с помощью JavaScript кода
+            JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
+            driver.manage().deleteAllCookies();
+            // указываем какой скрипт необходимо выполнить. Необходимо почистить все сессионные данные, которые
+            // которые находятся в определенном окне
+            javascriptExecutor.executeScript("windows.sessionStorage.clear()");
+        }
+    }
+
+    @AfterAll
+    // данный метод будет заниматься закрытием браузера. Для данного метода создадим в классе Config
+    // переменную HOLD_BROWSER_OPEN
+    void close() {
+
+    }
 
 }
